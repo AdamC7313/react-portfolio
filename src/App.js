cb71bd3683d58151a405e55986377023d9a9f362
AdamC7313 <AdamC7313@gmail.com>
2022-10-28 10:50:26 -0400

    complete home component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import Contact from './Contact/Contact';
 import Nav from './Nav/Nav';
+import Home from './Home/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
@@ -11,6 +13,9 @@ function App() {
       <h2 className='title'>Full Stack Web Developer</h2>
       <Contact />
       <Nav />
+      <Routes>
+        <Route path='/home' element={<Home />} />
+      </Routes>
     </div>
     </Router>
   );
---
+++ src/Home/Home.js
@@ -0,0 +1,11 @@
+import './Home.css';
+
+export default function Home() {
+    return(
+        <div class="home-container">
+            <span class="greeting"><h1>Hi, I'm Adam. </h1></span>
+            <span><h2 class="home-info">I'm an aspiring web developer learning a suite of languages and frameworks that will allow me to create beautiful and responsive websites 
+                and single-page applications.</h2></span>
+        </div>
+    )
+}
