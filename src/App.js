9a6351033c1fa4edeeddd0ccf8d8246c9882ba7e
AdamC7313 <AdamC7313@gmail.com>
2022-10-28 09:34:39 -0400

    complete contact component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Contact from './Contact/Contact';
 
 function App() {
   return (
     <div className="App">
+      <h1 className='name'>Adam Cruz</h1>
+      <h2 className='title'>Full Stack Web Developer</h2>
+      <Contact />
     </div>
   );
 }
---
+++ src/Contact/Contact.js
@@ -0,0 +1,23 @@
+import './Contact.css';
+import { BsLinkedin } from 'react-icons/bs';
+import { MdEmail } from 'react-icons/md';
+
+export default function Contact() {
+    return(
+    <div className="contact-container">
+        <div  className='contact-word'>
+        <h3>Contact:</h3>
+        </div>
+            <a href="https://www.linkedin.com/in/adamdcruz/" 
+            className="linkedin contact-button button" 
+            target="_blank">
+                <BsLinkedin />
+            </a>
+            <a href="mailto:AdamC7313@gmail.com" 
+            className="e-mail contact-button button" 
+            target="_blank">
+                <MdEmail />
+            </a>
+    </div>
+    )
+}
